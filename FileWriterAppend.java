bef7acfc3906530dc170fbdbfcaa8a8c4dbe801d
RetroWes <wlmorgan2001@gmail.com>
2023-11-09 20:32:08 -0500

    Implemented cleaner solution and added comments

--- FileWriterAppend.java
+++ FileWriterAppend.java
@@ -1,5 +1,19 @@
 import java.io.FileWriter;
 import java.io.File;
+import java.io.IOException;
+
+/*
+ * Author: RetroWes
+ * Date: 11/9/2023
+ * File: FileWriterAppend
+ * Description: This class contains a method for writing to a file.
+ * 
+ * -------
+ * Methods
+ * -------
+ * public static void writeToFile(String fileName, String str, boolean append): 
+ *      Writes {str} to {fileName}. If {append == true}, {str} is appended to {fileName}.
+ */
 
 class FileWriterAppend {
     public static void writeToFile(String fileName, String str, boolean append) {
@@ -7,17 +21,16 @@ class FileWriterAppend {
             File thisFile = new File(fileName);
             FileWriter thisFileWriter = null;
 
-            if (append) {
-                thisFileWriter = new FileWriter(thisFile, true);
-            } else {
-                thisFileWriter = new FileWriter(thisFile);
-            }
+            // If {append == true}, appends {str} to {fileName}. If {append == false}, text
+            // in {fileName} is overwritten with {str}.
+            thisFileWriter = new FileWriter(thisFile, append);
+
+            // The file is written to and the FileWriter object is closed to save resources.
             thisFileWriter.write(str);
             thisFileWriter.close();
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.getStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
