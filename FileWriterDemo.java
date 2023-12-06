bfec2f594daf1b325df99b66ab744e9de320e97f
MisterE <mistere12.coding@gmail.com>
2023-12-06 16:54:58 -0500

    Complete final instruction on exercise 1- catch errors with closing file access

--- FileWriterDemo.java
+++ FileWriterDemo.java
@@ -11,21 +11,28 @@ class FileWriterDemo {
             thisFile = new File(fileName);
             thisFileWriter = new FileWriter(thisFile);
             thisFileWriter.write(Str);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e);
             System.out.println("ERROR: Invalid input!");
             return;
-        }
-        finally{
-            if(thisFile != null){thisFileWriter.close();}
+        } finally {
+            // catch any errors preventing closing the file and alert the user.
+            try {
+                if (thisFile != null) {
+                    thisFileWriter.close();
+                }
+            } catch (Exception e) {
+                System.out.println("Error occured attempting to close File access!");
+            }
+
         }
 
         // System.out.printf("Content written to <%s> successfully.", fileName);
     }
+
     public static void main(String[] args) throws IOException {
         String fileName = "sample.txt";
         String sampleContent = " This is sample content";
-        writeToFile(fileName,sampleContent);
+        writeToFile(fileName, sampleContent);
     }
 }
