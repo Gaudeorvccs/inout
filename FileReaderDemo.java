ad39623784f53d5df9fca11605a484aeafb53232
MisterE <mistere12.coding@gmail.com>
2023-12-06 21:27:49 -0500

    Fix Exercise 3

--- FileReaderDemo.java
+++ FileReaderDemo.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /*
- * Author: RetroWes
+ * Author: RetroWes, Editor: Gaudeorvccs
  * Date: 2023-12-05
  * File: FileReaderDemo.java
  * Description: This class is for reading text from files.
@@ -20,21 +21,38 @@ import java.io.IOException;
  */
 
 class FileReaderDemo {
-    public static void readFromFile(String fileName) throws IOException {
-        BufferedReader reader = null;
-        reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));  // Initializes the reader with a file to read from.
-
-        String line;
-        while ((line = reader.readLine()) != null) {System.out.println(line);}              // {line} will be printed until {reader} is empty.
-        reader.close();                                                                     // Closes the reader to conserve resources.
-
-        throw(new IOException());                                                           // IOException is thrown intentionally for assignment purposes.
+    public static void exThrow() throws IOException {
+        throw new IOException();
     }
 
-    public static void main(String[] args) throws IOException {
-        try{readFromFile("sample.txt");                                            // Attempts to read from the file.
-        } catch (IOException e){
-            System.out.println("Error reading from file: Could Not Read From File.");     // Catches the IOException thrown in the method.
+    public static void readFromFile(String fileName) throws IOException {
+        // there are several lines of code that could throw an IOException. They will
+        // get propogated up to the main method. To test this fact, uncomment the
+        // exThrow call.
+
+        FileInputStream file = new FileInputStream(fileName); // COULD THROW FileNotFoundException
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file)); // Initializes the reader with a file
+                                                                                 // to read from.
+
+        String line;
+        while ((line = reader.readLine()) != null) { // readLine COULD THROW IOException
+            System.out.println(line);
+        } // {line} will be printed until {reader} is empty.
+        reader.close(); // Closes the reader to conserve resources. // COULD THROW IOException
+
+        // exThrow();
+        // IOException is thrown intentionally for
+        // testing purposes if the above line is uncommented.
+    }
+
+    public static void main(String[] args) {
+        try {
+            readFromFile("sample.txt"); // Attempts to read from the file.
+        } catch (FileNotFoundException e) {
+            System.out.println("Error reading from file: File does not exist.");
+        } catch (IOException e) {
+            System.out.println("Error reading from file: Could Not Read From File."); // Catches the IOException thrown
+                                                                                      // in the method.
         }
     }
 }
