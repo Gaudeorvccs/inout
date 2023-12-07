ccd3cc9d174a364165eb947fbf18c3b04e0eabdb
MisterE <mistere12.coding@gmail.com>
2023-12-07 09:48:43 -0500

    Implement Exercise 4

--- CSVFileDemo.java
+++ CSVFileDemo.java
@@ -34,9 +34,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+
+class CannotAccessException extends Exception {
+    public CannotAccessException(String msg) {
+        System.out.println("File Access Error: " + msg);
+    }
+}
 
 public class CSVFileDemo {
 
@@ -49,7 +58,26 @@ public class CSVFileDemo {
             }
         }
         line += "\n";
-        FileWriterAppend.writeToFile(fileName, line, append);
+
+        try {
+
+            File thisFile = new File(fileName);
+            FileWriter thisFileWriter = null;
+            try {
+                thisFileWriter = new FileWriter(thisFile, append);
+            } catch (IOException e) {
+                throw new CannotAccessException(
+                        "the file exists but is a directory rather than a regular file, does not exist but cannot be created, or cannot be opened for any other reason");
+            }
+            thisFileWriter.write(line);
+            thisFileWriter.close();
+        } catch (CannotAccessException e) {
+            System.out.println("IO error encountered with the CSV file: Cannot open file. ");
+        } catch (NullPointerException e) {
+            System.out.println("Error encountered with the CSV file: FileName argument is null.");
+        } catch (IOException e) {
+            System.out.println("IO error encountered with the CSV file: Cannot open file. ");
+        }
     }
 
     public static void writeCSV(String fileName, String[] inputStrs) {
@@ -61,28 +89,33 @@ public class CSVFileDemo {
     public static String[] getCSVLines(String fileName) throws IOException {
 
         BufferedReader reader = null;
+        String line;
+        String[] arrLines = null;
 
         // FileInputStream reads bytes from {fileName}.
         // InputStreamReader converts the bytes to characters.
         // BufferedReader supplies the readLine() method for outputting each line.
         try {
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+            // {line} will be printed until {reader} is empty.
+            ArrayList<String> lines = new ArrayList<String>();
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            arrLines = new String[lines.size()];
+            for (int i = 0; i < lines.size(); i++) {
+                arrLines[i] = lines.get(i);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: CSV file not found: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println(e);
-            System.out.println("ERROR: Invalid filepath!");
-        }
-
-        // {line} will be printed until {reader} is empty.
-        String line;
-        ArrayList<String> lines = new ArrayList<String>();
-        while ((line = reader.readLine()) != null) {
-            lines.add(line);
-        }
-        String[] arrLines = new String[lines.size()];
-        for (int i = 0; i < lines.size(); i++) {
-            arrLines[i] = lines.get(i);
+            System.out.println("IO error encountered with the CSV file: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("There was an error reading the file.");
         }
         return arrLines;
+
     }
 
     // splits a string at commas to return an array of Strings
@@ -108,7 +141,7 @@ public class CSVFileDemo {
 
     public static void main(String[] args) {
         // file name to write to
-        String fileName = "testCSV.csv";
+        String fileName = "Sample.csv";
         // write the test data
         writeCSV(fileName, new String[] { "Name", "Age", "Grade" });
         writeCSV(fileName, new String[] { "Alice", "23", "A" });
